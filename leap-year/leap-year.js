0e91ca1496efa453d917f07612689b6e82cc5ef5
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-10-28 10:50:38 -0700

    updated leap year added variables

--- leap-year/leap-year.js
+++ leap-year/leap-year.js
@@ -5,3 +5,20 @@ Make an app that determines if a given year is a leap year!
 
 Read the instructions in the README.md file in this folder.
 ------------------------------ */
+
+//ES Modules Syntax
+import moment from "moment";
+
+//commonJS syntax
+//const moment = require("moment");
+
+console.log("hello world!");
+
+const check2000 = moment([2000]).isLeapYear();
+console.log("is 2000 a leap year?", check2000);
+
+const check2001 = console.log(moment([2001]).isLeapYear()); // false
+console.log("is 2001 a leap year?", check2001);
+
+const check2100 = moment([2100]).isLeapYear(); // false
+console.log("is 2100 a leap year?", check2100);
